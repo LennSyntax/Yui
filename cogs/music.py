ba3ad5f41d727fef6640c20a57ab73f01d5e34d5
Vladlen <76092428+vladlen-codes@users.noreply.github.com>
2024-12-22 21:30:36 +0530

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -1,8 +1,7 @@
 import discord
 import wavelink
 from discord.ext import commands
-from discord.commands import Option
-from discord.commands import slash_command
+from discord.commands import Option, slash_command
 
 class Music(commands.Cog, name='music'):
     def __init__(self, bot):
@@ -11,7 +10,6 @@ class Music(commands.Cog, name='music'):
 
     async def node_connect(self):
         await self.bot.wait_until_ready()
-        await wavelink.NodePool.create_node(bot=self.bot, host='lavalink.oops.wtf', port=443, password='www.freelavalink.ga', https=True)
         await wavelink.NodePool.create_node(bot=self.bot, host='lavalink.oops.wtf', port=443, password='www.freelavalink.ga', https=True)
 
     @commands.Cog.listener()
@@ -25,65 +23,106 @@ class Music(commands.Cog, name='music'):
 
         if vc.loop:
             await vc.play(track)
-        next_song = vc.queue.get()
-        await vc.play(next_song)
-        embed = discord.Embed(description=f"<:music:991690031553073212> now playing: {next_song.title}", color=0xD684FF)
-        await ctx.send(embed=embed, delete_after=20)
+        else:
+            next_song = vc.queue.get()
+            await vc.play(next_song)
+            embed = discord.Embed(description=f"Now playing: {next_song.title}", color=0xD684FF)
+            await ctx.send(embed=embed, delete_after=20)
 
-    @slash_command(Name="play", description="Using this command you can play music.", guild_ids=None)
-    async def play(self, ctx, *, search: Option(str, description="Please enter your song name/artist name here.")):
+    @slash_command(name="play", description="Play music from YouTube.", guild_ids=None)
+    async def play(self, ctx, *, search: Option(str, description="Enter the song name or artist.")):
         search = await wavelink.YouTubeMusicTrack.search(query=search, return_first=True)
         if not ctx.voice_client:
-            vc: wavelink.Player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
-        elif not getattr(ctx.author.voice, "channel", None):
-            embed = discord.Embed(description="You have to join a voice channel first to play some music!", color=0xD684FF)
-            return await ctx.respond(embed=embed)
+            if ctx.author.voice:
+                vc: wavelink.Player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
+            else:
+                embed = discord.Embed(description="You need to join a voice channel first!", color=0xD684FF)
+                return await ctx.respond(embed=embed)
         else:
             vc: wavelink.Player = ctx.voice_client
 
         if vc.queue.is_empty and not vc.is_playing():
             await vc.play(search)
             embed = discord.Embed(color=0xD684FF)
-            embed.add_field(name="<:music:991690031553073212> now playing", value=f"{search.title}...")
-            return await ctx.respond(embed=embed)
+            embed.add_field(name="Now playing", value=f"{search.title}...")
+            await ctx.respond(embed=embed)
         else:
-            queue = vc.queue.copy()
-            number_count = 1
-            for number in queue:
-                number_count += 1
             await vc.queue.put_wait(search)
-            embed = discord.Embed(description=f"<:extra:991697564325924885> added {search.title}... to the queue - Position #{number_count}", color=0xD684FF)
+            embed = discord.Embed(description=f"Added {search.title} to the queue.", color=0xD684FF)
             await ctx.respond(embed=embed)
         vc.ctx = ctx
         setattr(vc, "loop", False)
 
-    @slash_command(Name="soundCloud", description="Using this command you can play SoundCloud links.", guild_ids=None)
-    async def soundcloud(self, ctx, *, search: Option(str, description="Please enter your song name/artist name here.")):
+    @slash_command(name="soundcloud", description="Play music from SoundCloud.", guild_ids=None)
+    async def soundcloud(self, ctx, *, search: Option(str, description="Enter the song name or artist.")):
         search = await wavelink.SoundCloudTrack.search(query=search, return_first=True)
         if not ctx.voice_client:
-            vc: wavelink.Player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
-        elif not getattr(ctx.author.voice, "channel", None):
-            embed = discord.Embed(description="You have to join a voice channel first to play some music!", color=0xD684FF)
-            return await ctx.respond(embed=embed)
+            if ctx.author.voice:
+                vc: wavelink.Player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
+            else:
+                embed = discord.Embed(description="You need to join a voice channel first!", color=0xD684FF)
+                return await ctx.respond(embed=embed)
         else:
             vc: wavelink.Player = ctx.voice_client
 
         if vc.queue.is_empty and not vc.is_playing():
             await vc.play(search)
             embed = discord.Embed(color=0xD684FF)
-            embed.add_field(name="<:music:991690031553073212> now playing", value=f"{search.title}...")
-            return await ctx.respond(embed=embed)
+            embed.add_field(name="Now playing", value=f"{search.title}...")
+            await ctx.respond(embed=embed)
         else:
-            queue = vc.queue.copy()
-            number_count = 1
-            for number in queue:
-                number_count += 1
             await vc.queue.put_wait(search)
-            embed = discord.Embed(description=f"<:extra:991697564325924885> added {search.title}... to the queue - Position {number_count}", color=0xD684FF)
-            await ctx.respond(embed=embed, allowed_mentions = discord.AllowedMentions(replied_user=False))
+            embed = discord.Embed(description=f"Added {search.title} to the queue.", color=0xD684FF)
+            await ctx.respond(embed=embed)
         vc.ctx = ctx
         setattr(vc, "loop", False)
-   
+
+    @slash_command(name="pause", description="Pause the current track.", guild_ids=None)
+    async def pause(self, ctx):
+        vc: wavelink.Player = ctx.voice_client
+        if vc.is_playing():
+            await vc.pause()
+            await ctx.respond("Paused the music.")
+        else:
+            await ctx.respond("No music is playing.")
+
+    @slash_command(name="resume", description="Resume the current track.", guild_ids=None)
+    async def resume(self, ctx):
+        vc: wavelink.Player = ctx.voice_client
+        if vc.is_paused():
+            await vc.resume()
+            await ctx.respond("Resumed the music.")
+        else:
+            await ctx.respond("Music is not paused.")
+
+    @slash_command(name="stop", description="Stop the current track.", guild_ids=None)
+    async def stop(self, ctx):
+        vc: wavelink.Player = ctx.voice_client
+        if vc.is_playing():
+            await vc.stop()
+            await ctx.respond("Stopped the music.")
+        else:
+            await ctx.respond("No music is playing.")
+
+    @slash_command(name="skip", description="Skip the current track.", guild_ids=None)
+    async def skip(self, ctx):
+        vc: wavelink.Player = ctx.voice_client
+        if vc.is_playing():
+            await vc.stop()
+            await ctx.respond("Skipped the track.")
+        else:
+            await ctx.respond("No music is playing.")
+
+    @slash_command(name="queue", description="Show the current music queue.", guild_ids=None)
+    async def queue(self, ctx):
+        vc: wavelink.Player = ctx.voice_client
+        if vc.queue.is_empty:
+            await ctx.respond("The queue is empty.")
+        else:
+            queue_list = "\n".join([f"{i+1}. {track.title}" for i, track in enumerate(vc.queue)])
+            embed = discord.Embed(title="Music Queue", description=queue_list, color=0xD684FF)
+            await ctx.respond(embed=embed)
+
 def setup(bot):
     bot.add_cog(Music(bot))
     print("Music cog is loaded!")
