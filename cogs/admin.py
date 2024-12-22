eddda89e14ee34cb07b3437a2597f00de556dceb
LennSyntax <104634443+LennSyntax@users.noreply.github.com>
2024-12-22 21:37:40 +0530

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -12,10 +12,10 @@ class Admin(commands.Cog, name='admin'):
 
     vc = discord.SlashCommandGroup("vc", "vc group", guild_ids=None)
 
-    @vc.command(description="You can create new voice channel using this.",)
+    @vc.command(description="Create a new voice channel.")
     @commands.has_guild_permissions(manage_channels=True)
     @commands.bot_has_guild_permissions(manage_channels=True)
-    async def channel(self, ctx, *, name: Option(str, description="You must enter the name of your channel here.")):
+    async def channel(self, ctx, *, name: Option(str, description="Enter the name of the channel.")):
         channel = await ctx.guild.create_voice_channel(name=name)
         embed = discord.Embed(description=f"{channel.name} voice channel has been created.", color=0xD684FF)
         await ctx.respond(embed=embed, delete_after=20)
@@ -23,17 +23,17 @@ class Admin(commands.Cog, name='admin'):
     @channel.error
     async def channel_error(self, ctx, error):
         if isinstance(error, commands.CheckFailure):
-            embed = discord.Embed(description=f"Oh no! you don't have the permission to run this commands.", color=0xD684FF)
+            embed = discord.Embed(description="You don't have permission to run this command.", color=0xD684FF)
             await ctx.respond(embed=embed, delete_after=10)
 
-    @vc.command(description="You can create new private voice channel using this.",)
+    @vc.command(description="Create a new private voice channel.")
     @commands.has_guild_permissions(manage_channels=True)
     @commands.bot_has_guild_permissions(manage_channels=True)
-    async def private(self, ctx, role: Option(discord.Role, description="Please mention the role you want the channel to have."), *, name: Option(str, description="You must enter the name of your channel here.")):
+    async def private(self, ctx, role: Option(discord.Role, description="Mention the role for the channel."), *, name: Option(str, description="Enter the name of the channel.")):
         overwrites = {
-        ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),
-        ctx.guild.me: discord.PermissionOverwrite(read_messages=True),
-        role: discord.PermissionOverwrite(read_messages=True)
+            ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),
+            ctx.guild.me: discord.PermissionOverwrite(read_messages=True),
+            role: discord.PermissionOverwrite(read_messages=True)
         }
         channel = await ctx.guild.create_voice_channel(name=name, overwrites=overwrites)
         embed = discord.Embed(description=f"{channel.name} private voice channel has been created.", color=0xD684FF)
@@ -42,14 +42,12 @@ class Admin(commands.Cog, name='admin'):
     @private.error
     async def private_error(self, ctx, error):
         if isinstance(error, commands.CheckFailure):
-            embed = discord.Embed(description=f"Oh no! you don't have the permission to run this commands.", color=0xD684FF)
+            embed = discord.Embed(description="You don't have permission to run this command.", color=0xD684FF)
             await ctx.respond(embed=embed, delete_after=10)
 
-
-    @vc.command(description="You can delete a voice channel using this command.")    
+    @vc.command(description="Delete a voice channel.")    
     @commands.bot_has_guild_permissions(manage_channels=True)
-    async def delete(self, ctx, *, channel: Option(discord.VoiceChannel, description="Please mention the channel you want to delete.")):
-        channel = channel or ctx.channel
+    async def delete(self, ctx, *, channel: Option(discord.VoiceChannel, description="Mention the channel to delete.")):
         await channel.delete()
         embed = discord.Embed(description=f"{channel.name} voice channel has been deleted.", color=0xD684FF)
         await ctx.respond(embed=embed, delete_after=20)
@@ -57,164 +55,133 @@ class Admin(commands.Cog, name='admin'):
     @delete.error
     async def delete_error(self, ctx, error):
         if isinstance(error, commands.CheckFailure):
-            embed = discord.Embed(description=f"Oh no! you don't have the permission to run this commands.", color=0xD684FF)
+            embed = discord.Embed(description="You don't have permission to run this command.", color=0xD684FF)
             await ctx.respond(embed=embed, delete_after=10)
  
-    @vc.command(description="Kicks the VC Rules Breaker.")
+    @vc.command(description="Kick a user from the voice channel.")
     @commands.has_permissions(kick_members=True)
     @commands.bot_has_guild_permissions(kick_members=True)
-    async def kick(self,ctx,member: Option(discord.Member, description="Please mention the user you wanna mute here.", required = True)):
-        asdf = ctx.author
-        f = member.top_role
-        h = asdf.top_role
-        if h > f or ctx.guild.owner == ctx.author and not member == ctx.author:
-            if member.guild_permissions.ban_members and not ctx.guild.owner == ctx.author:
-                embed = discord.Embed(description=f"Something went wrong please try again.", color=0xD684FF)
+    async def kick(self, ctx, member: Option(discord.Member, description="Mention the user to kick.", required=True)):
+        if ctx.author.top_role > member.top_role or ctx.guild.owner == ctx.author:
+            if member.guild_permissions.ban_members and ctx.guild.owner != ctx.author:
+                embed = discord.Embed(description="Something went wrong, please try again.", color=0xD684FF)
                 await ctx.respond(embed=embed, delete_after=10)
             else: 
                 await member.edit(voice_channel=None)
-                await ctx.respond(f"{member.mention} was kicked in the VC.")
+                await ctx.respond(f"{member.mention} was kicked from the VC.")
         else:
-            if member == ctx.author:
-                embed = discord.Embed(description=f"You can't kick yourself. ¯\_(ツ)_/¯", color=0xD684FF)
-                await ctx.respond(embed=embed, delete_after=10)
-            else:
-                embed = discord.Embed(description=f"Hmm don't try to break, that person has a higher or equal role to you.", color=0xD684FF)
-                await ctx.respond(embed=embed, delete_after=10)
+            embed = discord.Embed(description="You can't kick this user.", color=0xD684FF)
+            await ctx.respond(embed=embed, delete_after=10)
 
     @kick.error
     async def kick_error(self, ctx, error):
         if isinstance(error, commands.CheckFailure):
-            embed = discord.Embed(description=f"Oh no! you don't have the permission to run this commands.", color=0xD684FF)
+            embed = discord.Embed(description="You don't have permission to run this command.", color=0xD684FF)
             await ctx.respond(embed=embed, delete_after=10)
 
-    @vc.command(description="Mutes the specified user in the VC.")
+    @vc.command(description="Mute a user in the voice channel.")
     @commands.has_permissions(manage_messages=True)
     @commands.bot_has_guild_permissions(manage_messages=True)
-    async def mute(self,ctx,member: Option(discord.Member, description="Please mention the user you wanna mute here.", required = True)):
-        asdf = ctx.author
-        f = member.top_role
-        h = asdf.top_role
-        if h > f or ctx.guild.owner == ctx.author and not member == ctx.author:
-            if member.guild_permissions.ban_members and not ctx.guild.owner == ctx.author:
-                embed = discord.Embed(description=f"Something went wrong please try again.", color=0xD684FF)
+    async def mute(self, ctx, member: Option(discord.Member, description="Mention the user to mute.", required=True)):
+        if ctx.author.top_role > member.top_role or ctx.guild.owner == ctx.author:
+            if member.guild_permissions.ban_members and ctx.guild.owner != ctx.author:
+                embed = discord.Embed(description="Something went wrong, please try again.", color=0xD684FF)
                 await ctx.respond(embed=embed, delete_after=10)
             else: 
-                guild = ctx.guild
                 await member.edit(mute=True)
                 await ctx.respond(f"{member.mention} was muted in the VC.")
         else:
-            if member == ctx.author:
-                embed = discord.Embed(description=f"You can't mute yourself. ¯\_(ツ)_/¯", color=0xD684FF)
-                await ctx.respond(embed=embed, delete_after=10)
-            else:
-                embed = discord.Embed(description=f"Hmm don't try to break, that person has a higher or equal role to you.", color=0xD684FF)
-                await ctx.respond(embed=embed, delete_after=10)
+            embed = discord.Embed(description="You can't mute this user.", color=0xD684FF)
+            await ctx.respond(embed=embed, delete_after=10)
          
     @mute.error
     async def mute_error(self, ctx, error):
         if isinstance(error, commands.CheckFailure):
-            embed = discord.Embed(description=f"Oh no! you don't have the permission to run this commands.", color=0xD684FF)
+            embed = discord.Embed(description="You don't have permission to run this command.", color=0xD684FF)
             await ctx.respond(embed=embed, delete_after=10)
 
-    @vc.command(description="Unmutes the specified user in the VC.")
+    @vc.command(description="Unmute a user in the voice channel.")
     @commands.has_permissions(manage_messages=True)
     @commands.bot_has_guild_permissions(manage_messages=True)
-    async def unmute(self,ctx,member: Option(discord.Member, description="Please mention the user you wanna unmute here.", required = True)):
-        asdf = ctx.author
-        f = member.top_role
-        h = asdf.top_role
-        if h > f or ctx.guild.owner == ctx.author and not member == ctx.author:
-            if member.guild_permissions.ban_members and not ctx.guild.owner == ctx.author:
-                embed = discord.Embed(description=f"Something went wrong please try again.", color=0xD684FF)
+    async def unmute(self, ctx, member: Option(discord.Member, description="Mention the user to unmute.", required=True)):
+        if ctx.author.top_role > member.top_role or ctx.guild.owner == ctx.author:
+            if member.guild_permissions.ban_members and ctx.guild.owner != ctx.author:
+                embed = discord.Embed(description="Something went wrong, please try again.", color=0xD684FF)
                 await ctx.respond(embed=embed, delete_after=10)
             else: 
                 await member.edit(mute=False)
                 await ctx.respond(f"{member.mention} was unmuted in the VC.")
         else:
-            if member == ctx.author:
-                embed = discord.Embed(description=f"You were never muted and you can't unmute yourself.", color=0xD684FF)
-                await ctx.respond(embed=embed, delete_after=10)
-            else:
-                embed = discord.Embed(description=f"Hmm don't try to break, that person has a higher or equal role to you.", color=0xD684FF)
-                await ctx.respond(embed=embed, delete_after=10)
-         
+            embed = discord.Embed(description="You can't unmute this user.", color=0xD684FF)
+            await ctx.respond(embed=embed, delete_after=10)
 
     @unmute.error
     async def unmute_error(self, ctx, error):
         if isinstance(error, commands.CheckFailure):
-            embed = discord.Embed(description=f"Oh no! you don't have the permission to run this commands.", color=0xD684FF)
+            embed = discord.Embed(description="You don't have permission to run this command.", color=0xD684FF)
             await ctx.respond(embed=embed, delete_after=10)
 
-    @vc.command(description="Lock a particular voice channel.")
+    @vc.command(description="Lock the current voice channel.")
     @commands.has_permissions(manage_channels=True)
     @commands.bot_has_guild_permissions(manage_channels=True)
     async def lock(self, ctx):
-        if(ctx.author.voice):
+        if ctx.author.voice:
             channel = ctx.author.voice.channel
             overwrite = channel.overwrites_for(ctx.guild.default_role)
             overwrite.connect = False
             await channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
-            embed = discord.Embed(description=f"{ctx.author.voice.channel.name} has been locked 🔒", color=0xD684FF)
+            embed = discord.Embed(description=f"{channel.name} has been locked 🔒", color=0xD684FF)
             await ctx.respond(embed=embed, delete_after=20)
 
     @lock.error
     async def lock_error(self, ctx, error):
         if isinstance(error, commands.CheckFailure):
-            embed = discord.Embed(description=f"Oh no! you don't have the permission to run this commands.", color=0xD684FF)
+            embed = discord.Embed(description="You don't have permission to run this command.", color=0xD684FF)
             await ctx.respond(embed=embed, delete_after=10)
 
-    @vc.command(description="Unlock a particular voice channel.")
+    @vc.command(description="Unlock the current voice channel.")
     @commands.has_permissions(manage_channels=True)
     @commands.bot_has_guild_permissions(manage_channels=True)
     async def unlock(self, ctx):
-        if(ctx.author.voice):
+        if ctx.author.voice:
             channel = ctx.author.voice.channel
             overwrite = channel.overwrites_for(ctx.guild.default_role)
             overwrite.connect = True
             await channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
-            embed = discord.Embed(description=f"{ctx.author.voice.channel.name} has been unlocked 🔓", color=0xD684FF)
+            embed = discord.Embed(description=f"{channel.name} has been unlocked 🔓", color=0xD684FF)
             await ctx.respond(embed=embed, delete_after=20)
 
     @unlock.error
     async def unlock_error(self, ctx, error):
         if isinstance(error, commands.CheckFailure):
-            embed = discord.Embed(description=f"Oh no! you don't have the permission to run this commands.", color=0xD684FF)
+            embed = discord.Embed(description="You don't have permission to run this command.", color=0xD684FF)
             await ctx.respond(embed=embed, delete_after=10)
     
-    @slash_command(Name="VC Deafen", description="Deafen the specified user in the VC.", guild_ids=None)
+    @slash_command(name="VC Deafen", description="Deafen a user in the voice channel.", guild_ids=None)
     @commands.has_permissions(manage_messages=True)
     @commands.bot_has_guild_permissions(manage_messages=True)
-    async def deafen(self,ctx,member: Option(discord.Member, description="Please mention the user you wanna deafen.", required = True)):
-        asdf = ctx.author
-        f = member.top_role
-        h = asdf.top_role
-        if h > f or ctx.guild.owner == ctx.author and not member == ctx.author:
-            if member.guild_permissions.ban_members and not ctx.guild.owner == ctx.author:
-                embed = discord.Embed(description=f"Something went wrong please try again.", color=0xD684FF)
+    async def deafen(self, ctx, member: Option(discord.Member, description="Mention the user to deafen.", required=True)):
+        if ctx.author.top_role > member.top_role or ctx.guild.owner == ctx.author:
+            if member.guild_permissions.ban_members and ctx.guild.owner != ctx.author:
+                embed = discord.Embed(description="Something went wrong, please try again.", color=0xD684FF)
                 await ctx.respond(embed=embed, delete_after=10)
             else: 
-                guild = ctx.guild
                 await member.edit(deafen=True)
-                await ctx.respond(f"{member.mention} was deafen in the VC.")
+                await ctx.respond(f"{member.mention} was deafened in the VC.")
         else:
-            if member == ctx.author:
-                embed = discord.Embed(description=f"You can't deafen yourself. ¯\_(ツ)_/¯", color=0xD684FF)
-                await ctx.respond(embed=embed, delete_after=10)
-            else:
-                embed = discord.Embed(description=f"Hmm don't try to break, that person has a higher or equal role to you.", color=0xD684FF)
-                await ctx.respond(embed=embed, delete_after=10)
+            embed = discord.Embed(description="You can't deafen this user.", color=0xD684FF)
+            await ctx.respond(embed=embed, delete_after=10)
          
     @deafen.error
     async def deafen_error(self, ctx, error):
         if isinstance(error, commands.CheckFailure):
-            embed = discord.Embed(description=f"Oh no! you don't have the permission to run this commands.", color=0xD684FF)
+            embed = discord.Embed(description="You don't have permission to run this command.", color=0xD684FF)
             await ctx.respond(embed=embed, delete_after=10)
     
-    @slash_command(Name="VC Undeafen", description="Undeafen the specified user in the VC.", guild_ids=None)
+    @slash_command(name="VC Undeafen", description="Undeafen a user in the voice channel.", guild_ids=None)
     @commands.has_permissions(manage_messages=True)
     @commands.bot_has_guild_permissions(manage_messages=True)
-    async def undeafen(self,ctx,member: Option(discord.Member, description="Please mention the user you wanna undeafen.", required = True)):
+    async def undeafen(self, ctx, member: Option(discord.Member, description="Mention the user to undeafen.", required=True)):
         asdf = ctx.author
         f = member.top_role
         h = asdf.top_role
